6964a5666b8d768c94b8a4c6a8f3c3c8d20333bc
KalabSisay <kalab1275@gmail.com>
2022-01-30 22:50:00 +0300

    IP

---
+++ resources/scripts/script.js
@@ -0,0 +1,9 @@
+// Focus = Changes the background color of input to red
+function focusFunction() {
+    document.getElementById("position").style.background = "red";
+}
+
+// No focus = Changes the background color of input to green
+function blurFunction() {
+    document.getElementById("position").style.background = "green";
+}
